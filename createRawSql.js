196f23ea621e7040737be31c641e8dd146789d08
samantha frangi <sefrangi13@gmail.com>
2022-03-09 20:33:38 -0600

    removes sql files, makes changes to products table

--- createRawSql.js
+++ createRawSql.js
@@ -8,7 +8,7 @@ const products = await productList.json()
 function createSQLquery(list) {
     unlink('./wrongly-quoted-products.sql', err => { if (err) console.info(err) })
     list.forEach(product => {
-        appendFile('./wrongly-quoted-products.sql', `INSERT INTO product (id, brand, product_name, price, website_link, tags) values("${product.id}","${product.brand}","${product.name}","${product.price}","${product.website_link}", "${product.tag_list}");\n`, (err) => {
+        appendFile('./wrongly-quoted-products.sql', `INSERT INTO product (id, brand, product_name, price, website_link, tags,image_link, product_description, product_type,product_colors) values("${product.id}","${product.brand}","${product.name}","${product.price}","${product.website_link}", "${product.tag_list}","${product.image_link}","${product.description}","${product.product_type}","${product.product_colors}" );\n`, (err) => {
             if (err) {
                 console.error(err)
             }
