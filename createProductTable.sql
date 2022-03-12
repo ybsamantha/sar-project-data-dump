cca68739d1ea70146642f0b7707021714581d481
samantha frangi <sefrangi13@gmail.com>
2022-03-12 11:42:10 -0600

    finalizes set up for seed db

--- createProductTable.sql
+++ createProductTable.sql
@@ -1,1 +1,3 @@
-CREATE TABLE product(id int, brand VARCHAR(150), product_name VARCHAR(150), price NUMERIC, website_link VARCHAR(150), tags VARCHAR(150), image_link VARCHAR, product_description VARCHAR(4000), product_type VARCHAR(150), product_colors text[][]);
+CREATE TABLE product(id int UNIQUE, brand VARCHAR(150), product_name VARCHAR(150), price NUMERIC, website_link VARCHAR(150), tags VARCHAR(150), image_link VARCHAR, product_description VARCHAR(4000), product_type VARCHAR(150));
+
+CREATE TABLE color(product_id int REFERENCES product (id), hex_value VARCHAR(7), colour_name VARCHAR(150), color_id SERIAL PRIMARY KEY);
--- createRawSql.js
+++ createRawSql.js
@@ -6,19 +6,24 @@ let productList = await fetch('http://makeup-api.herokuapp.com/api/v1/products.j
 const products = await productList.json()
 
 function createSQLquery(list) {
-    
+
     unlink('./wrongly-quoted-products.sql', err => { if (err) console.info(err) })
     list.forEach(product => {
-        let color_data = [] 
         product.product_colors.forEach(color => {
-        let data = `"${color.hex_value}", "${color.colour_name}"`
-            color_data.push(data)
+            appendFile('./wrongly-quoted-products.sql', `INSERT INTO color(product_id, hex_value, colour_name) values(${product.id}, "${color.hex_value}", "${color.colour_name}");\n`, (err) => {
+                if (err) {
+                    console.error(err)
+                }
+            })
         })
-        appendFile('./wrongly-quoted-products.sql', `INSERT INTO product (id, brand, product_name, price, website_link, tags,image_link, product_description, product_type,product_colors) values("${product.id}","${product.brand}","${product.name}","${product.price}","${product.website_link}", "${product.tag_list}","${product.image_link}","${product.description}","${product.product_type}","[${color_data}]" );\n`, (err) => {
+
+        appendFile('./wrongly-quoted-products.sql', `INSERT INTO product (id, brand, product_name, price, website_link, tags,image_link, product_description, product_type) values("${product.id}","${product.brand}","${product.name}","${product.price}","${product.website_link}", "${product.tag_list}","${product.image_link}","${product.description}","${product.product_type}");\n`, (err) => {
             if (err) {
                 console.error(err)
             }
+
         })
+
     })
 }
 
